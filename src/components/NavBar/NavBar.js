2c0050c042e8d8cb5ea8f8a4b5b6350be3e52ed0
Confaa <juanconfaa@gmail.com>
2020-12-10 14:43:13 -0300

    Correccion NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,91 @@
+import "./NavBar.css";
+
+let NavBar = () => {
+    return (
+        <header>
+            <div class="container">
+                <section>
+                    <a href="index.html">
+                        <h2>Store Ingelec</h2>
+                    </a>
+
+                    <a href="#">
+                        <div class="carritoNavBar">
+                            <p class="d-none d-md-block">Carrito de compras</p>
+                            <i class="fas fa-shopping-cart"></i>
+                        </div>
+                    </a>
+                </section>
+
+                <a href="#" class="toggle-button">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </a>
+
+                <nav>
+                    <ul class="navBar">
+                        <li class="desplegable">
+                            Motherboards<i class="fas fa-angle-down"></i>
+                            <ul>
+                                <li>
+                                    <a href="views/mothersAmd.html">P/AMD</a>
+                                </li>
+                                <li>
+                                    <a href="views/mothersIntel.html">P/Intel</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="desplegable">
+                            Procesadores<i class="fas fa-angle-down"></i>
+                            <ul>
+                                <li>
+                                    <a href="views/cpusAmd.html">AMD</a>
+                                </li>
+                                <li>
+                                    <a href="views/cpusIntel.html">Intel</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="views/rams.html">Memorias Ram</a>
+                        </li>
+                        <li>
+                            <a href="views/gpus.html">Placas de Video</a>
+                        </li>
+                        <li>
+                            <a href="views/storage.html">Almacenamiento</a>
+                        </li>
+                        <li>
+                            <a href="views/monitors.html">Monitores</a>
+                        </li>
+                        <li class="desplegable">
+                            Otros<i class="fas fa-angle-down"></i>
+                            <ul>
+                                <li>
+                                    <a href="views/computerCases.html">Gabinetes</a>
+                                </li>
+                                <li>
+                                    <a href="views/coolers.html">Coolers P/CPU</a>
+                                </li>
+                                <li>
+                                    <a href="views/os.html">Sistemas Operativos</a>
+                                </li>
+                                <li>
+                                    <a href="#">Lectora DVD/Blu-Ray</a>
+                                </li>
+                                <li>
+                                    <a href="#">Perifericos</a>
+                                </li>
+                                <li>
+                                    <a href="views/powerSupplies.html">Fuentes de Energia</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
+export default NavBar;
