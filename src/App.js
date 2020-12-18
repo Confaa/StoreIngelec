601a2fe8ededffc5ffa46cac027263f6d994ae0f
Confaa <juanconfaa@gmail.com>
2020-12-17 21:26:10 -0300

    ItemCount

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.scss";
 import NavBar from "./components/NavBar/NavBar.js";
 import Home from "./components/Home/Home.js";
+import ItemCount from "./components/ItemCount/ItemCount.js";
 
 function App() {
+    const handleAdd = () => {};
     return (
         <div className="container-fluid">
             <NavBar />
             <Home />
+            <ItemCount onAdd={handleAdd} initialValue={0} maxValue={5} />
         </div>
     );
 }
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,44 @@
+import { React, useState } from "react";
+import "./ItemCount.scss";
+
+const ItemCount = ({ initialValue, maxValue, onAdd }) => {
+    const [contador, setContador] = useState(initialValue);
+    const [stock, setStock] = useState(maxValue);
+
+    const aumentarContador = () => {
+        if (stock > 0 && contador < 5) {
+            setContador(contador + 1);
+            setStock(stock - 1);
+        } else {
+            document.getElementById("demo").innerHTML = "No queda stock";
+        }
+    };
+
+    const restarContador = () => {
+        if (contador > 0 && stock < 6) {
+            setContador(contador - 1);
+            setStock(stock + 1);
+            document.getElementById("demo").innerHTML = "";
+        } else if (contador == 5) {
+            setContador(contador - 1);
+            setStock(stock + 1);
+        }
+    };
+
+    return (
+        <div className="contador">
+            <p> Elementos: {contador} </p>
+            <button className="elemento" onClick={aumentarContador}>
+                {" "}
+                Agregar elemento{" "}
+            </button>
+            <button className="elemento" onClick={restarContador}>
+                {" "}
+                Quitar elemento
+            </button>
+            <p id="demo"></p>
+        </div>
+    );
+};
+
+export default ItemCount;
