9505fab59a524c2148a1c97ddf530a4584bb9e2a
Juan Confalonieri <juanconfaa@gmail.com>
2021-01-09 20:59:39 -0300

    Structure Folder

---
+++ src/layout/Main/ItemDetailContainer/ItemDetail/ItemDetail.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./ItemDetail.scss";
+import ItemCount from "../../../../components/ItemCount/ItemCount";
+import { Link } from "react-router-dom";
+let ItemDetail = ({ prod }) => {
+    const [cant, setCant] = useState(false);
+
+    const onAdd = (e) => {
+        e.stopPropagation();
+        setCant(e.target.value);
+    };
+    console.log(cant);
+
+    return (
+        <span id="ItemDetail">
+            <img src={prod.img} alt=""></img>
+            <p>Titulo: {prod.title}</p>
+            <p>Marca: {prod.brand}</p>
+
+            {prod.category === "Accionamientos" ? (
+                <p>Corriente Nominal: {prod.ratedCurrent} A</p>
+            ) : prod.category === "Motores" ? (
+                <>
+                    <p>Potencia: {prod.powerage} HP</p>
+                    <p>Polos: {prod.poles}</p>
+                </>
+            ) : prod.category === "Cables" ? (
+                <p>Seccion: {prod.section} mm.</p>
+            ) : null}
+
+            <p>Descripcion: {prod.description}</p>
+            <p>Stock: {prod.stock}</p>
+            <p>Precio: {prod.price}$</p>
+            <p>Codigo de producto: {prod.id}</p>
+            {!cant ? (
+                <ItemCount initialValue={1} maxValue={prod.stock} onAdd={onAdd} />
+            ) : (
+                <Link to="/cart" className="terminarCompra">
+                    Terminar compra
+                </Link>
+            )}
+        </span>
+    );
+};
+
+export default ItemDetail;
---
+++ src/layout/Main/Main.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Productos from "../../views/Productos/Productos";
+import Cuenta from "../../views/Cuenta/Cuenta";
+import ItemListContainer from "../../components/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "../../components/ItemDetailContainer/ItemDetailContainer";
+import "./Main.scss";
+
+const Main = () => {
+    return (
+        <main>
+            <Switch>
+                <Route path="/" exact>
+                    <ItemListContainer />
+                </Route>
+                <Route path="/category/:id" exact>
+                    <ItemListContainer />
+                </Route>
+                <Route path="/category/:id/:subid" exact>
+                    <ItemListContainer />
+                </Route>
+                <Route path="/Productos">
+                    <Productos />
+                </Route>
+                <Route path="/Cuenta">
+                    <Cuenta />
+                </Route>
+                <Route path="/item/:id">
+                    <ItemDetailContainer />
+                </Route>
+            </Switch>
+        </main>
+    );
+};
+
+export default Main;
